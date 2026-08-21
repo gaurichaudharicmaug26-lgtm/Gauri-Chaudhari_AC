bc80a98f5ec5c8c925f16078e88492d31d1a3fa7
Gauri Chaudhari_AC <gaurichaudhari.cmaug26@gmail.com>
2026-08-21 18:43:27 +0530

    Add files via upload

---
+++ PositiveNumber.java
@@ -0,0 +1,9 @@
+class PositiveNumber{
+    public static void main(String[]args) {
+	    int n = 10;
+		if (n > 0)
+System.out.println("Positive");
+        else
+		    System.out.println("Not Positive");
+	}
+}
