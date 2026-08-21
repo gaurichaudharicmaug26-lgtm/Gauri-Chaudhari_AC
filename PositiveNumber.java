4f7c10ef69c02fb4a4e02a05940873c249ab1e12
Gauri Chaudhari_AC <gaurichaudhari.cmaug26@gmail.com>
2026-08-21 18:48:22 +0530

    Delete PositiveNumber.java

--- PositiveNumber.java
+++
@@ -1,9 +0,0 @@
-class PositiveNumber{
-    public static void main(String[]args) {
-	    int n = 10;
-		if (n > 0)
-System.out.println("Positive");
-        else
-		    System.out.println("Not Positive");
-	}
-}
